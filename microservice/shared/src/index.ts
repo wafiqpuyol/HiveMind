6eb3f84aad454d4b45b159ae0819d44a35d6b244
wafiq <wafiqgithub775@gmail.com>
2024-05-04 14:20:46 +0600

    add all requried interface for shared library
    This interface will be used by frontend & backend by importing the
    shared library

---
+++ microservice/shared/src/index.ts
@@ -0,0 +1,72 @@
+export {
+  IAuth,
+  IAuthBuyerMessageDetails,
+  IAuthDocument,
+  IAuthPayload,
+  IAuthResponse,
+  IAuthUser,
+  IEmailMessageDetails,
+  IForgotPassword,
+  ISignInPayload,
+  ISignUpPayload,
+  IReduxAddAuthUser,
+  IReduxAuthPayload,
+  IReduxLogout,
+  IResetPassword,
+} from './interface/auth.interface';
+export { IBuyerDocument, IReduxBuyer } from './interface/buyer.interface';
+export {
+  IChatBoxProps,
+  IChatBuyerProps,
+  IChatMessageProps,
+  IChatSellerProps,
+  IConversationDocument,
+  IMessageDetails,
+  IMessageDocument,
+} from './interface/chat.interface';
+export { IEmailLocals } from './interface/email.interface';
+export {
+  GigType,
+  ICreateGig,
+  IGigCardItems,
+  IGigContext,
+  IGigInfo,
+  IGigTopProps,
+  IGigViewReviewsProps,
+  IGigsProps,
+  ISellerGig,
+  ISelectedBudget,
+} from './interface/gig.interface';
+export {
+  IDeliveredWork,
+  IExtendedDelivery,
+  IOffer,
+  IOrderDocument,
+  IOrderEvents,
+  IOrderMessage,
+  IOrderReview,
+  IOrderNotifcation,
+} from './interface/order.interface';
+export {
+  IRatingCategories,
+  IRatingCategoryItem,
+  IRatingTypes,
+  IReviewDocument,
+  IReviewMessageDetails,
+} from './interface/review.interface';
+export {
+  IHitsTotal,
+  IPaginateProps,
+  IQueryList,
+  IQueryString,
+  ISearchResult,
+  ITerm,
+} from './interface/search.interface';
+export {
+  ICertificate,
+  IEducation,
+  IExperience,
+  ILanguage,
+  ISellerDocument,
+  SellerType,
+} from './interface/seller.interface';
